2d044afd7ce80b842eb6a7e09449554726a1c059
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-09-09 21:46:26 +0000

    classwork

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,11 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        double num1 = 57.3934;
+        double num2 = 22.5211;
 
+        int sum;
+        sum = num1
+    }
+}
