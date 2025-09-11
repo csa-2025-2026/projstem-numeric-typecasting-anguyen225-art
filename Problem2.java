738d9e344270def7af0f9a2153762679071a40a9
anguyen225-art <anguyen225@student.hbuhsd.edu>
2025-09-11 21:07:09 +0000

    classwork

--- Problem2.java
+++ Problem2.java
@@ -4,8 +4,7 @@ public class Problem2
     {
         double num1 = 57.3934;
         double num2 = 22.5211;
-
-        int sum;
-        sum = num1
+        int sum = (int)(num1 + 0.5) + (int)(num2 + 0.5);
+        System.out.println(sum);
     }
 }
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,11 @@
-
+public class Problem3
+{
+    public static void main(String[] args)
+    {
+       double num = 67.3424;
+       double first3 = num * 1000;
+       int intnum = (int)first3;
+       int x = intnum % 1000;
+       System.out.print((x / 100) + " " + ((x/10) % 10) + " " + (x % 10));
+    }
+}
